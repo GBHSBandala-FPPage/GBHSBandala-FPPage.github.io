a7ffb263188c1391c30f523e422f1cb70e8f0156
Irfan-Sattar10 <108290289+Irfan-Sattar10@users.noreply.github.com>
2022-07-05 05:47:46 -0700

    Create JScripts.js

---
+++ JScripts.js
@@ -0,0 +1,19 @@
+<!-- Facebook Default Java Script Codes-->
+<script>
+  window.fbAsyncInit = function() {
+    FB.init({
+      appId      : '394510922632347',
+      xfbml      : true,
+      version    : 'v14.0'
+    });
+    FB.AppEvents.logPageView();
+  };
+
+  (function(d, s, id){
+     var js, fjs = d.getElementsByTagName(s)[0];
+     if (d.getElementById(id)) {return;}
+     js = d.createElement(s); js.id = id;
+     js.src = "https://connect.facebook.net/en_US/sdk.js";
+     fjs.parentNode.insertBefore(js, fjs);
+   }(document, 'script', 'facebook-jssdk'));
+</script>
